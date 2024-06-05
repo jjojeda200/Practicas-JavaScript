cf615e26a0c2f79b99fd930c08581e549879424c
José Juan Ojeda Granados <jotafirebase@gmail.com>
2024-06-05 18:41:41 +0100

    ok

--- promesas00.js
+++ promesas00.js
@@ -1,26 +1,26 @@
-  const miPromesa = new Promise((resolve, reject) => {
-    // Lógica asíncrona o tareas
-    const exito = true;
-  
-    if (exito) {
-      esperar(2000).then(() => {
-        resolve("Operación exitosa");
-      });
-    } else {
-      reject(new Error("Algo salió mal"));
-    }
-  });
+const miPromesa = new Promise((resolve, reject) => {
+  // Lógica asíncrona o tareas
+  const exito = true;
 
-  miPromesa
+  if (exito) {
+    esperar(2000).then(() => {
+      resolve("Operación exitosa");
+    });
+  } else {
+    reject(new Error("Algo salió mal"));
+  }
+});
+
+miPromesa
   .then((resultado) => {
-    console.log(resultado);       // Operación exitosa
+    console.log(resultado); // Operación exitosa
   })
   .catch((error) => {
     console.error(error.message); // Algo salió mal
   });
 
-  function esperar(ms) {
-    return new Promise(resolve => {
-      setTimeout(resolve, ms);
-    });
-  }
+function esperar(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
